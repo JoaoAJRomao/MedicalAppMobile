a1e7f2e0e3362ecdb98ad393df10a4fe26cd9a1a
Joao Romao <tupi_1800@hotmail.com>
2022-04-13 00:29:40 -0300

    Try debugger

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StyleSheet, SafeAreaView } from 'react-native';
 import Routes from './src/Routes';
 
 export default function App() {
+  if (__DEV__) {
+    require('react-devtools');
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Routes/>
--- src/components/TelaLogin/Login.jsx
+++ src/components/TelaLogin/Login.jsx
@@ -25,6 +25,7 @@ export default function Login() {
       pwd: pwd
     }
     const cmd = await LogarCliente(data)
+    console.log(cmd)
 
   }
 
