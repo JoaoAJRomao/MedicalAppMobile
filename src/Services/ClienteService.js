57e6e3ed9bae07d906224c4f44806607b3e5c86a
João Arthur <tupi_1800@hotmail.com>
2022-05-06 21:56:10 -0300

    Test

--- src/Services/ClienteService.js
+++ src/Services/ClienteService.js
@@ -2,40 +2,40 @@ import api from "./api"
 
 
 export async function CriarCliente(params) {
-    const estrutura = []
-  
-    await api.post(`/cliente/createClient`, params)
-      .then((res) => {
-        estrutura.push(res.data.data)
-      }).catch((err) => {
-        estrutura.push(err.data.data)
-      });
+  const estrutura = []
 
-    return estrutura
+  await api.post(`/cliente/createClient`, params)
+    .then((res) => {
+      estrutura.push(res.data.data)
+    }).catch((err) => {
+      estrutura.push(err.data.data)
+    });
+
+  return estrutura
 }
 
 export async function LogarCliente(params) {
-    const estrutura = []
-  
-    await api.post(`/cliente/login`, params)
-      .then((res) => {
-        estrutura.push(res?.data)
-      }).catch((err) => {
-        estrutura.push(err?.response)
-      });
+  const estrutura = []
 
-    return estrutura
+  await api.post(`/cliente/login`, params)
+    .then((res) => {
+      estrutura.push(res?.data)
+    }).catch((err) => {
+      estrutura.push(err?.response)
+    });
+
+  return estrutura
 }
 
 export async function RecuperarSenha(params) {
-    const estrutura = []
-  
-    await api.post(`/cliente/RecoverEmail?email=${params}`)
-      .then((res) => {
-        estrutura.push(res?.data)
-      }).catch((err) => {
-        estrutura.push(err?.response)
-      });
+  const estrutura = []
 
-    return estrutura
+  await api.post(`/cliente/RecoverEmail?email=${params}`)
+    .then((res) => {
+      estrutura.push(res?.data)
+    }).catch((err) => {
+      estrutura.push(err?.response)
+    });
+
+  return estrutura
 }
