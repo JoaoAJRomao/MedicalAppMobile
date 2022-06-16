cc3070126f638d5bfe66e80ebfd2306643b1731b
João Arthur <tupi_1800@hotmail.com>
2022-06-16 15:42:03 -0300

    Filtra caracteres em campo nome

--- src/components/TelaCadastroCliente/CadastroCliente.jsx
+++ src/components/TelaCadastroCliente/CadastroCliente.jsx
@@ -13,84 +13,97 @@ export default function CadastroCliente() {
   const [textDataNascimento, setTextDataNascimento] = useState(null);
   const [selectPlano, setSelectPlano] = useState(null);
 
-  return (
-  <NativeBaseProvider>
-  <View style={styles.container}>
-    <ScrollView
-      showsVerticalScrollIndicator={false}
-      style={{ marginBottom: 20 }}>
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          marginBottom: 30,
-          marginTop: 30
-        }}
-      >
-        <Image
-          style={styles.tinyLogo}
-          source={require("@expo/../../assets/MedicalAppIcon2.png")}
-        />
-      </View>
-      <MaskedTextInput
-        style={styles.input}
-        onChangeText={setTextCpf}
-        placeholder=" Digite seu CPF"
-        mask="999.999.999-99"
-        value={textCpf}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setTextNome}
-        placeholder=" Digite seu Nome Completo"
-        value={textNome}
-        maxLength={50}
-        keyboardType="ascii-capable"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setTextSenha}
-        placeholder=" Digite sua senha"
-        secureTextEntry={true}
-        maxLength={20}
-        value={textSenha}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setTextConfirmacao}
-        placeholder=" Confirme sua senha"
-        secureTextEntry={true}
-        maxLength={20}
-        value={textConfirmacao}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setTextEmail}
-        placeholder=" Digite seu e-mail"
-        keyboardType="email-address"
-        maxLength={40}
-        value={textEmail}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={setTextDataNascimento}
-        placeholder=" Digite sua Data de Nascimento"
-        value={textDataNascimento}
-      />
-      
-      
-    <Select onValueChange={itemValue => setSelectPlano(itemValue)} style={{backgroundColor: '#FFFF'}}>
-     <Select.Item label="Hapvida" value="hapvida" />
-     <Select.Item label="Unimed" value="unimed" />
-     <Select.Item label="Bradesco" value="bradesco" />
-     </Select> 
+  function filterInput(text) {
+    const specialCharacters = /[`~0-9!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/g;
+    if (specialCharacters.test(text)) {
+      return setTextNome(text.replace(specialCharacters, ''));
+    }
+    setTextNome(text)
+  }
 
-      <TouchableOpacity style={styles.buttonCadastro}>
-        <Text style={styles.textCadastro}>Criar Conta</Text>
-      </TouchableOpacity>
-    </ScrollView>
-  </View>
-  </NativeBaseProvider>);
+  return (
+    < NativeBaseProvider >
+      <View style={styles.container}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={{ marginBottom: 20 }}>
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: 30,
+              marginTop: 30
+            }}
+          >
+            <Image
+              style={styles.tinyLogo}
+              source={require("@expo/../../assets/MedicalAppIcon2.png")}
+            />
+          </View>
+          <MaskedTextInput
+            style={styles.input}
+            onChangeText={setTextCpf}
+            placeholder=" Digite seu CPF"
+            mask="999.999.999-99"
+            value={textCpf}
+          />
+
+
+          <TextInput
+            style={styles.input}
+            onChangeText={text => filterInput(text)}
+            placeholder=" Digite seu Nome Completo"
+            value={textNome}
+            maxLength={50}
+            keyboardType="ascii-capable"
+          />
+
+
+          <TextInput
+            style={styles.input}
+            onChangeText={setTextSenha}
+            placeholder=" Digite sua senha"
+            secureTextEntry={true}
+            maxLength={20}
+            value={textSenha}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={setTextConfirmacao}
+            placeholder=" Confirme sua senha"
+            secureTextEntry={true}
+            maxLength={20}
+            value={textConfirmacao}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={setTextEmail}
+            placeholder=" Digite seu e-mail"
+            keyboardType="email-address"
+            maxLength={40}
+            value={textEmail}
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={setTextDataNascimento}
+            placeholder=" Digite sua Data de Nascimento"
+            value={textDataNascimento}
+          />
+
+
+          <Select onValueChange={itemValue => setSelectPlano(itemValue)} style={{ backgroundColor: '#FFFF' }}>
+            <Select.Item label="Hapvida" value="hapvida" />
+            <Select.Item label="Unimed" value="unimed" />
+            <Select.Item label="Bradesco" value="bradesco" />
+          </Select>
+
+          <TouchableOpacity style={styles.buttonCadastro}>
+            <Text style={styles.textCadastro}>Criar Conta</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </NativeBaseProvider>
+  );
 }
 
 
